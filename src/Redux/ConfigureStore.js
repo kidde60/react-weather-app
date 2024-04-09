e8856320b1380862ff1743b7701f453dc31bb66b
kidde60 <kgeorgewilliam60@gmail.com>
2024-04-09 15:55:23 +0300

    Display api data and add Css

---
+++ src/Redux/ConfigureStore.js
@@ -0,0 +1,8 @@
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import homeReducer from "./Home/home";
+
+const reducer = combineReducers({ homeReducer });
+const store = createStore(reducer, applyMiddleware(logger, thunk));
+export default store;
