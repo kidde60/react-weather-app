c6040277622ac26dcf1892f437283181ba7f168b
GEORGE WILLIAM KIDDE <kgeorgewilliam60@gmail.com>
2024-04-11 18:10:46 +0300

    Update Cities.js

--- src/components/home/Cities.js
+++ src/components/home/Cities.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import { v4 as uuid } from "uuid";
 import { getDataCity } from "../../Redux/Home/home";
 import egypt from "../image/resize.jpg";
-import Nav from "./Nav";
 
 const CITYS = () => {
   const dispatch = useDispatch();
